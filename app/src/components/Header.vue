92be9a4b4d3b6dae6942b35d8b2c761309a75f8f
Ander_AAM <ander_2030@hotmail.com>
2021-08-22 05:36:22 -0500

    Vista Principal .Slider

---
+++ app/src/components/Header.vue
@@ -0,0 +1,99 @@
+<template>
+    <b-container class="header_1" fluid>
+        <b-container class='P-bienvenida'>
+            <p>
+                <br>
+                Bienvenidos al Centro Comercial Virtual
+            </p>
+        </b-container>
+        <b-container class='search'>
+            <form class="form d-flex">
+                <input class="busqueda form-control me-2" type="search" placeholder="Busca aquí" aria-label="Search">
+                <i class="fas fa-search"></i>
+            </form>
+        </b-container>
+        <b-container>
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-whatsapp"></i>
+        </b-container>
+    </b-container>
+</template>
+<script type="text/javascript">
+export default {
+    name: 'Header'
+}
+</script>
+<style type="text/css">
+.header_1 {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    justify-self: center;
+    align-content: center;
+    height: 60px;
+    background-color: rgb(88, 25, 147) !important;
+}
+
+.header_1 form input {
+    position: relative;
+    width: 110%;
+}
+
+.header_1 .P-bienvenida {
+    position: relative;
+    width: 40%;
+    left: -3%;
+}
+
+.header_1 div p {
+    font-size: 1.5vw;
+    color: white !important;
+}
+
+.header_1 div i {
+    font-size: 1.9vw;
+}
+
+.header_1 div {
+    position: relative;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 50px;
+    width: 33%;
+}
+
+.header_1 .form {
+    position: relative;
+    width: 100%;
+}
+
+.header_1 .busqueda {
+    position: relative;
+    outline: none;
+    background-color: rgb(75, 19, 128);
+    border: 1px solid rgb(75, 19, 128);
+}
+
+.header_1 .busqueda::placeholder {
+    color: white !important;
+}
+
+
+.header_1 form i {
+    position: relative;
+    font-size: 1.5vw;
+    left: -15%;
+    top: 7px;
+}
+
+
+.header_1 i {
+    position: relative;
+    padding-left: 1.2vw;
+    color: white !important;
+    cursor: pointer;
+}
+</style>
---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,79 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="Logo navbar-brand" href="index.html"><img src="/src/assets/img/Logo.nav.JPG" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav m-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Quienes Somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Afiliados</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Planes.html">Planes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tiendas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Marketplace</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Contacto.html">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+<style>
+    .navbar {
+    position: relaive;
+    width: 100%;
+    height: 90px;
+    background-color: white !important;
+}
+
+.navbar .Logo {
+    position: relative;
+    padding-left: -10px;
+    width: 14%;
+    z-index: 50;
+}
+
+nav .Logo img {
+    position: relative;
+    left: 50%;
+    height: 50px;
+}
+
+
+.navbar div,
+.navbar ul {
+    position: relative;
+    background-color: rgb(255, 255, 255) !important;
+    z-index: 10;
+}
+
+.navbar ul li {
+    position: relative;
+    font-size: 1.2vw;
+    margin-left: 15px;
+
+}
+</style>
+<script type="text/javascript">
+export default {
+    name: 'Nabar'
+}
+</script>
